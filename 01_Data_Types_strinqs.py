ad84d85b0f69aa4cba6d260565b74372a670f86a
Ah feng <afeng999999999@gmail.com>
2024-11-24 01:35:14 +0700

    Add files via upload

---
+++ 01_Data_Types_strinqs.py
@@ -0,0 +1,14 @@
+
+# 02 Data Types Strings --> ข้อความ, ตัวอักษร
+
+Name = "Best" # B e s t --> Strings --> ตัวอักษร ข้อความ
+
+Coding = "python" # --> Srtings --> ตัวอักษร ข้อความ
+
+Country = "thai" # --> Srtings --> ตัวอักษร ข้อความ
+
+print(Name) # --> Normal Display
+print(Coding) # --> Normal Display
+print(Country) # --> Normal Display
+
+print("My name is" + " " + Name + " " + Coding)
