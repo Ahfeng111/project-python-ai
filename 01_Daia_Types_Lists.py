2a8bc26dc3a131cdf03308fc1b0da9d58889fe93
Ah feng <afeng999999999@gmail.com>
2024-11-24 01:31:52 +0700

    Add files via upload

---
+++ 01_Daia_Types_Lists.py
@@ -0,0 +1,31 @@
+
+# 03 Data Types Lists --> ชุดข้อมูล
+
+Name = "Best" #--> Single
+
+Number_01 = 10 # Single
+
+
+Mixed_Name = ["Best", "meimei", "n", "win", "Milk"]
+
+Mixed_Number = [1,2, 1.12, 56, 45.65656565]
+
+Mixed_Data = ["Best", 1, "meimei", -16]
+
+
+print(Mixed_Name) #--> Normal Display
+
+Mixed_Name.sort()
+
+print(Mixed_Name) #--> Normal Display
+
+# print(Mixed_Data) #--> Normal Display
+# print(Mixed_Number[5]) #--> Normal Display
+
+# print("My name is" + " " + Mixed_Name[3])
+
+
+
+
+
+
